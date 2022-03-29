8832965899bd333fa16b1b143503215c08280e14
mathieu-tatat <mathieu.tatat@laplateforme.io>
2022-03-29 17:33:27 +0200

    add branch for hackaton

--- Controller/produit_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once ('Model/Produit.php');
-    $produit = new Produits();
-
-    if(isset($_POST['update_prod']))
-    {
-           $produit->updateProduit();    
-    }
--- View/Error.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if (isset($_SESSION['errors']) && count($_SESSION['errors']) > 0) : ?>
-    <div>
-        <?php foreach ($_SESSION['errors'] as $error) : ?>
-            <h5 class="text-center text-danger"><?= $error ?></h5>
-        <?php endforeach ?>
-    </div>
-<?php  endif; ?>
